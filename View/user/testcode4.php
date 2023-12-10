9476dfec6c00b55b076b5d77c9f0930ad71eec58
subway90 <nguyenhieu3105@gmail.com>
2023-12-10 23:50:27 +0700

    Gắn template vào case testcode4

---
+++ View/user/testcode4.php
@@ -0,0 +1,17 @@
+<?php
+echo"Source code của chức năng testcode4";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template version 2.0</title>
+</head>
+<body>
+    <h1>Header</h1>
+    <h1>Chức năng testcode 4</h1>
+    <h2>Body</h2>
+    <h3>Footer</h3>
+</body>
+</html>
